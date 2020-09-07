326497f3d3887ddeb5fbf1439b95650d2b4c97b5
Mcprince12 <sohumtrip@gmail.com>
2020-09-07 11:00:16 +0200

    we started the car racing game

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var hypnoticBall, database;
-var position;
+var database;
+var form, game, player;
+var gameState = 0;
+var playerCount;
 
 
 function setup(){
@@ -7,47 +9,13 @@ function setup(){
   console.log(database);
   createCanvas(500,500);
 
-  hypnoticBall = createSprite(250,250,10,10);
-  hypnoticBall.shapeColor = "red";
+  game = new Game();
 
-
-  var hypnoticBallPosition = database.ref('ball/position');
-  hypnoticBallPosition.on("value", readPosition, showError);
+  game.getState();
+  game.start();
 }
 
 function draw(){
-  background("white");
-  
-    if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
-    }
-    drawSprites();
-  
+
 }
 
-function writePosition(x,y){
-  database.ref('ball/position').set({
-    'x': position.x + x ,
-    'y': position.y + y
-  })
-}
-
-function readPosition(data){
-  position = data.val();
-  console.log(position.x);
-  hypnoticBall.x = position.x;
-  hypnoticBall.y = position.y;
-}
-
-function showError(){
-  console.log("Error in writing to the database");
-}
